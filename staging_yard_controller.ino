fb684bd64c46e237cafa1e87e2f7f7528576ebeb
Marcus Wong <marcusthomaswong@gmail.com>
2023-01-17 21:12:48 +1100

    First cut of code - two track sensors, two servos to control points, two track power outputs

---
+++ staging_yard_controller.ino
@@ -0,0 +1,95 @@
+#include <Servo.h>
+
+int TRACK1_POWER = 10;
+int TRACK2_POWER = 11;
+
+Servo myservo1;
+Servo myservo2;  // create servo object to control a servo
+
+void setup() {
+
+  // turn power off
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  digitalWrite(TRACK1_POWER,HIGH);
+  digitalWrite(TRACK2_POWER,HIGH);
+  
+  myservo1.attach(0);  // attaches the servo on pin 9 to the servo object
+  myservo2.attach(1);  // attaches the servo on pin 9 to the servo object
+
+  // reset servo location  
+  myservo1.write(180); 
+  myservo2.write(180); 
+  delay(500);
+  
+  myservo1.write(0); 
+  myservo2.write(0); 
+  delay(500);
+    
+  bool track1occupied = !digitalRead(A0);
+  bool track2occupied = !digitalRead(A1);
+
+  if (track1occupied)
+  {
+    trackGo1();    
+  }
+  else if (track2occupied)
+  {
+    trackGo2();    
+  }  
+}
+
+void trackGo1()
+{
+  digitalWrite(TRACK1_POWER,LOW);
+  myservo1.write(180); 
+  delay(10000);  
+  resetTracks();
+}
+
+void trackGo2()
+{
+  digitalWrite(TRACK2_POWER,LOW);
+  myservo2.write(180); 
+  delay(10000);  
+  resetTracks();
+}
+
+void resetTracks()
+{
+  // turn power off
+  digitalWrite(TRACK1_POWER,HIGH);
+  digitalWrite(TRACK2_POWER,HIGH);
+  
+  myservo1.write(0);     
+  myservo2.write(0); 
+}
+
+void loop() {
+
+  bool track1occupied = !digitalRead(A0);
+  bool track2occupied = !digitalRead(A1);
+  
+  // 1 = no trains
+  // 0 = covered = train
+  // Serial.print("track 1 = ");
+  // Serial.println(track1occupied ? "occupied" : "clear");
+  // Serial.print("track 2 = ");
+  // Serial.println(track2occupied ? "occupied" : "clear");
+
+  if (track1occupied && !track2occupied)
+  {
+    trackGo1();
+  } 
+  else if (track2occupied && !track1occupied)
+  {
+    trackGo2();
+  }
+  else
+  {
+   // Serial.println("Do nothing!");
+    delay(1000);
+  }
+  
+
+}
